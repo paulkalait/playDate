40792d4be4c67d0855b57e9a754835f265cb6d4b
Your Name <kalaitzidispaul@gmail.com>
2022-07-17 18:50:13 -0400

    added button styling

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState} from 'react'
+import './styles.css'
+import Filebase from 'react-file-base64'
 
 const Form = () => {
+  const [postData, setPostData] = useState({
+    name: '',
+    title: '',
+    //will be a dropdown
+    size: '',
+    message: '',
+    tags: '',
+    selectedFile: ''
+  })
+  const handleSubmit = () => {
+
+  }
   return (
-    <div>
-  <h1>Form</h1>
+    <div className='formContainer'>
+      <form autoComplete='off' noValidate onSubmit={handleSubmit}>
+        <h1>Post your companion</h1>                                                                            {/* use spread operator to only affect name in this field*/}
+        <input name='name' variant='outlined' placeholder="Name" value={postData.name} onChange={(e) => setPostData({...postData, name: e.target.value})}/>
+        <input name='title' variant='outlined' placeholder="Title" value={postData.title} onChange={(e) => setPostData({...postData, name: e.target.value})}/>
+        <input name='message' variant='outlined' placeholder="Message" value={postData.message} onChange={(e) => setPostData({...postData, name: e.target.value})}/>
+        <input name='tags' variant='outlined' placeholder="Tags" value={postData.tags} onChange={(e) => setPostData({...postData, name: e.target.value})}/>
+
+        <select value={postData.size} onChange={(e) => setPostData({...postData, size: e.target.value})}>
+        <option>small</option>
+        <option>medium</option>
+        <option>large</option>
+        </select>
+        <div>
+          <Filebase
+          type="file"
+          multiple={false}
+          onDone={({base64}) => setPostData({...postData, selectedFile: base64})} />
+        </div>
+
+        <div className='buttonContainer'>
+        <button className='submit-button'>Submit</button>
+        <button className='clear-button'>Clear</button>
+        </div>
+     
+      </form>
     </div>
   )
 }
