a78b20316859662cfedd1a4d997e12c30ceb1ff7
Your Name <kalaitzidispaul@gmail.com>
2022-07-17 17:59:39 -0400

    fixed configureStore

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import Post from './Post/Post.js'
+import { useSelector } from 'react-redux'
 import './styles.css'
 
 const Posts = () => {
+      // get access to the global state in the reducers /posts.js file ..
+  const posts = useSelector((state) =>  state.posts)
+
+  console.log(posts)
   return (
     <div className='postsContainer'>
     <h1>Posts</h1>
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux'
-import { configureStore, applyMiddleWare, compose} from 'redux'
+import { configureStore, applyMiddleware, compose} from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
 import reducers from './reducers';
 //initiate redux
 
-const store = configureStore(reducers, compose(applyMiddleWare(thunk)))
+      //configureStore
+const store = configureStore({reducer: reducers}, compose(applyMiddleware(thunk)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
