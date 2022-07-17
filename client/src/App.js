f1ea87e147f88c4c68820d7838dd90cd3a0b7a7b
Your Name <kalaitzidispaul@gmail.com>
2022-07-17 17:09:40 -0400

    started frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
-import './App.css';
+
+
+import dog from './assets/images/dog.svg'
+import Posts from '../src/components/Posts/Posts'
+import Form from '../src/components/Form/Form'
+import './App.css'
+
 
 function App() {
+
+
   return (
-    <div className="App">
-    
+ <div>
+    <header className='header'>
+      <h1>playDate</h1>
+      <img src={dog} alt="doglogo" />
+    </header>
+
+    <div className='father'>
+    <div className='div-for-posts-and-form'>
+
+     <div className='Posts-container'>
+      <Posts />
+     </div>
+     <div className='Form-container'>
+      <Form />
+     </div>
     </div>
+    </div>
+ 
+ </div>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -2,9 +2,15 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import postRoutes from './routes/posts.js'
 
 //initialize this app
 const app = express();
+
+//use middleware for the prefix routes th post routes will be reached bu localhost:5000/posts/ ....
+app.use('/posts', postRoutes)
+
+
 
 //set up body parser
 app.use(bodyParser.json({limit: "30mb", extended: true}))
@@ -13,7 +19,5 @@ app.use(cors())
 
 //host data base thorugh the cloud 
 const MONGODB_URI = 'mongodb+srv://memories:memories@cluster0.ruxvc.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true}).then(() => app.listen(PORT, () => console.log(`server is running on part: ${PORT}`))).catch((error) => console.log(error))
-
-mongoose.set('useFindAndModify', false)
+const PORT = process.env.PORT || 3001;
+mongoose.connect(MONGODB_URI).then(() => app.listen(PORT, () => console.log(`server is running on part: ${PORT}`))).catch((error) => console.log(error))
