dfe6863e50be707a1b9213102c4533c5e73571f4
Your Name <kalaitzidispaul@gmail.com>
2022-08-12 16:52:41 +0200

    proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(
-      createProxyMiddleware(["/api", "/otherApi"], { target: "http://localhost:3001" })
-    );
-    }
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:3001',
+      changeOrigin: true,
+    })
+  );
+};
