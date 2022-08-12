01a038fd767ea0499cf16a126c88343d0605a63b
Your Name <kalaitzidispaul@gmail.com>
2022-08-12 16:12:37 +0200

    proxy

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ const PORT = process.env.PORT || 3001;
 
 mongoose
   .connect(
-    "mongodb+srv://memories:memories@cluster0.ruxvc.mongodb.net/?retryWrites=true&w=majority"
+process.env.MONGODB_URI
   )
   .then(() =>
     app.listen(PORT, () => console.log(`server is running on port: ${PORT}`))
