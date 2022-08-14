9119f75f54fd7760df1005a1449295aecaaa90aa
Your Name <kalaitzidispaul@gmail.com>
2022-08-14 21:07:35 +0200

    dep

--- server/index.js
+++ server/index.js
@@ -42,6 +42,7 @@ app.get('*', (request, response) => {
   response.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
