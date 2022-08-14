89c0e9c59911dd2c082509bc9bc70d1590ff765b
Your Name <kalaitzidispaul@gmail.com>
2022-08-14 15:29:22 +0200

    dep

---
+++ client/build/precache-manifest.456871a0b89e8fd3328b1268d8769603.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81834e649dfcb3072d44d3e869ee9160",
+    "url": "/index.html"
+  },
+  {
+    "revision": "141dd5a7eb1fe1471b46",
+    "url": "/static/css/main.5a35fb80.chunk.css"
+  },
+  {
+    "revision": "99ca35ab8808f90db49c",
+    "url": "/static/js/2.fd5d1015.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.fd5d1015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "141dd5a7eb1fe1471b46",
+    "url": "/static/js/main.092eafe4.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.092eafe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "1e1f89f59de699e1ba68cefc9077f007",
+    "url": "/static/media/Social.1e1f89f5.svg"
+  },
+  {
+    "revision": "895853c26b3d08907aeeff0c8295cec4",
+    "url": "/static/media/account-logo.895853c2.svg"
+  },
+  {
+    "revision": "60e40e037f0cf2f2bc8409bbe879c905",
+    "url": "/static/media/auth-login-walking.60e40e03.svg"
+  },
+  {
+    "revision": "7d5397ecb3b66e08b2e5fe65eedd82c5",
+    "url": "/static/media/background.7d5397ec.svg"
+  },
+  {
+    "revision": "aa0ec174087802e5217f86c6992937ab",
+    "url": "/static/media/comment.aa0ec174.svg"
+  },
+  {
+    "revision": "014361bd1d5b81b40cbddbd58ba7dfe9",
+    "url": "/static/media/doglogo.014361bd.png"
+  },
+  {
+    "revision": "08aab0b474633adedc15359d227df5ca",
+    "url": "/static/media/giveTreat.08aab0b4.svg"
+  },
+  {
+    "revision": "2ff7b68b3bb0402f5f6bb0eaac3358d6",
+    "url": "/static/media/home-logo.2ff7b68b.png"
+  },
+  {
+    "revision": "d29fc1472197b547fc964509f93a9aec",
+    "url": "/static/media/lock.d29fc147.svg"
+  },
+  {
+    "revision": "2d1aaf7807a8467b6d30cc9877a77bca",
+    "url": "/static/media/relaxing-dog-walk.2d1aaf78.svg"
+  }
+]);
